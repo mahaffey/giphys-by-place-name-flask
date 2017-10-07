2a9e7e74ec832a1162da2eac8438c9cc51fd28bc
mahaffey <ryan.a.mahaffey@gmail.com>
2017-10-06 21:44:12 -0400

    set

--- server.py
+++ server.py
@@ -1,5 +1,9 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
+
+
+if __name__ == '__main__':
+    app.run()
