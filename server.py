b874cee98f71ee5eef1b685a9b90375b41b9382a
mahaffey <ryan.a.mahaffey@gmail.com>
2017-10-06 21:51:42 -0400

    created

--- server.py
+++ server.py
@@ -3,7 +3,13 @@ from flask import Flask, jsonify, request, render_template
 app = Flask(__name__)
 
 @app.route("/")
+def home_page():
+    return render_template("./views/index.html")
 
+@app.route("/", methods=["POST"])
+def form_posting():
+    place = request.form['text']
+    return place
 
 if __name__ == '__main__':
     app.debug = True
