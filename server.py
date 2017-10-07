34b58b6c7572962b58f9b2faa6650c072f597843
mahaffey <ryan.a.mahaffey@gmail.com>
2017-10-07 05:02:16 -0400

    first

--- server.py
+++ server.py
@@ -23,7 +23,6 @@ def home_page():
 @app.route('/', methods=['POST'])
 def form_posting():
     # check to see if request is coming from HTML form or cURL
-
     if request.form: # HTML form
         return web_page_form_submition(request)
 
@@ -39,66 +38,59 @@ def web_page_form_submition(request):
 
     # check to see what type of search user is performing
     if request.form['submit'] == 'Search for a Gif':
-        return get_one_place(query)
+        return get_places(query, 'one')
     elif request.form['submit'] == 'Search for many Gifs':
-        return get_many_places(query)
+        return get_places(query, 'many')
 
 def curl_json_submition(request):
     # decode Byte object to JSON string then convert to JSON object
-    json_data = json.loads(request.data.decode('utf8'))
+    json_data = json.loads(request.data.decode('utf-8'))
     # set query from JSON data
     query = json_data['query']
     # get search request type ('one' or 'many')
     request_type = json_data['type']
 
-    # check to see what type of search user is performing
-    if request_type == 'one':
-        return get_one_place(query)
-    elif request_type == 'many':
-        return get_many_places(query)
+    return get_places(query, request_type)
+
 
 # method to hit google places api
 def fetch_google_places_api(query):
     api_key = 'AIzaSyDoH3nXDk5CGtBpgjkmuvcCJ1U2EfEixw8'
     url = 'https://maps.googleapis.com/maps/api/place/textsearch/json?query={0}&key={1}'.format(query, api_key)
-
+    # embed()
     # fetches with this url and sets response as response
     with urllib.request.urlopen(url) as response:
         data = response.read()
         return data
 
-def get_one_place(query):
-    data = fetch_google_places_api(query)
+def get_places(query, search_type):
+    encoded_query = '+'.join(query.split()).encode('utf-8').strip()
+    data = fetch_google_places_api(encoded_query)
     to_json = json.loads(data)
-    # create random number to select a place from places search result
-    # make sure we can't go over the index size by using len
-    length = len(to_json['results'])
-    index = random.randrange(0,length)
-    place = to_json['results'][index]
+    results = to_json['results']
 
-    # the code below formats all json in easy to read fashion to see printout in console
-    # pretty_json = json.dumps(to_json, sort_keys=True, indent=4)
-    # print(pretty_json)
+    # check what type of search user is performing
+    if search_type == 'one':
+        length = len(results)
+        index = random.randrange(0,length)
+        place = results[index]
+        name = place['name']
+        encoded_name = '+'.join(name.split()).encode('utf-8').strip()
 
-    name = place['name']
-    name_no_spaces = '+'.join(name.split())
+        giphy_data = json.loads(fetch_giphy(encoded_name))
+        place['giphy'] = giphy_data
+        return jsonify(place)
 
-    giphy_data = json.loads(fetch_giphy(name_no_spaces))
+    elif search_type == 'many':
+        places = results
+        for place in places:
+            name = place['name']
+            encoded_name = '+'.join(name.split()).encode('utf-8').strip()
+            
+            giphy_data = json.loads(fetch_giphy(encoded_name))
+            place['giphy'] = giphy_data
 
-    place['giphy'] = giphy_data
-
-    return jsonify(place)
-
-def get_many_places(query):
-    data = fetch_google_places_api(query)
-    to_json = json.loads(data)
-    places = to_json['results']
-
-    # the code below formats all json in easy to read fashion to see printout in console
-    # pretty_json = json.dumps(to_json, sort_keys=True, indent=4)
-    # print(pretty_json)
-
-    return jsonify(places)
+        return jsonify(places)
 
 def fetch_giphy(query):
     api_key = 'qOMNTU8uPW8fGbZlT7L1Ueu9hzXLb6QD'
